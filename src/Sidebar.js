8839d44911666621aebbaf9c3edff67796ef6769
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-10-22 10:41:55 +0300

    updated sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import { FaTree, FaSignOutAlt, FaRegSun, FaRegQuestionCircle, FaUserTie } from 'react-icons/fa';
+import { FaTree, FaSignOutAlt, FaRegSun, FaRegQuestionCircle, FaUserTie, FaHouseUser, FaInfoCircle } from 'react-icons/fa';
 import MyCalendar from './CalendarComponent';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
+      <div className='sidebar-title'>
         <FaTree/>
         <span>Forest Guard</span>
-
-
+      </div>
 
         <div className='sidebar-links'>
           <div className='sidebar-top-links'>
-          <MyCalendar />
           <a href='/' className='sidebar-link mt'>
-                <FaUserTie />
-                <span>Reach out to authorities</span>
+                <FaHouseUser className='side-icons' />
+                <span>Home</span>
                 </a>
+          <a href='/' className='sidebar-link mt'>
+                <FaInfoCircle className='side-icons' />
+                <span>About</span>
+                </a>
+          <a href='/' className='sidebar-link mt'>
+                <FaUserTie className='side-icons' />
+                <span>Reach out to authorities</span>
+                </a> 
           </div>
-          
             <div className='sidebar-bottom-links'>
             <a href='/' className='sidebar-link'>
-                <FaRegQuestionCircle />
+                <FaRegQuestionCircle className='side-icons' />
                 <span>Help and support</span>
                 </a>
             <a href='/' className='sidebar-link'>
-                <FaRegSun />
+                <FaRegSun className='side-icons' />
                 <span>Settings</span>
                 </a>    
                 <a href='/' className='sidebar-link logout-link'>
-                <FaSignOutAlt />
+                <FaSignOutAlt className='side-icons' />
                 <span>Logout</span>
                 </a>
             </div>
